09fd209f28aec6d830a17c6310221302833c3163
alierzakazemeini <alierzakazemeini@ce.sharif.edu>
2022-04-16 22:25:42 +0430

    commit -m

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class Main {
 
     public static void main(String[] args) {
-
+//        Vector<Boolean> playing
     }
 }
--- src/main/java/Models/Game.java
+++ src/main/java/Models/Game.java
@@ -1,5 +1,6 @@
 package Models;
 
+import Controllers.GameMenuController;
 import Models.Tiles.Tile;
 
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ public class Game {
     private final ArrayList<User> users;
     private final Vector<User> userTurn;
     private ArrayList<Tile> tilesMap;
-
+    private GameMenuController controller;
     public Game(ArrayList<User> users) {
         this.users = users;
         this.tilesMap = new ArrayList<>();
@@ -17,6 +18,7 @@ public class Game {
         for (int i = users.size() - 1; i > -1; i--) {
             this.userTurn.add(users.get(i));
         }
+        this.controller = new GameMenuController(this);
     }
 
     private void generateMap() {
