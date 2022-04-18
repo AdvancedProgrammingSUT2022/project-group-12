49f3eddd5abf1d19a90e7eee474babf01a09f7a6
alierzakazemeini <alierzakazemeini@ce.sharif.edu>
2022-04-18 18:28:17 +0430

    game menu controller changed

--- src/main/java/Controllers/GameMenuController.java
+++ src/main/java/Controllers/GameMenuController.java
@@ -1,18 +1,21 @@
 package Controllers;
 
 import Enums.CommandResponseEnum;
+import Models.Civilization;
 import Models.Game;
 import Models.Tiles.TileGrid;
 import Models.Units.Unit;
 
 public class GameMenuController {
     private final Game game;
+    private Civilization player1;
+    private Civilization player2;
 
     public GameMenuController(Game newGame) {
         this.game = newGame;
     }
 
-    public CommandResponseEnum battle(Unit Attacking, Unit defending) {
+    public CommandResponseEnum battle(Civilization attacking, Civilization defending) {
         return CommandResponseEnum.OK;
     }
 
