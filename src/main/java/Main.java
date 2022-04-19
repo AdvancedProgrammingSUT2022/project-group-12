1892a6075d94185382212b310541c76f2cc570ad
Alireza Askarian <ar.asgarian1389@gmail.com>
2022-04-19 13:36:49 +0430

    new commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import Controllers.Command;
+import Exceptions.InvalidCommand;
 import Views.LoginMenu;
 import Views.MenuStack;
 
@@ -5,10 +7,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidCommand {
         Scanner scanner = new Scanner(System.in);
         MenuStack menuStack = MenuStack.getInstance();
         menuStack.setScanner(scanner);
+        System.out.println(Command.parseCommand("username jief  jfi -u ali asghar --fjaiee aioefj"));
         menuStack.pushMenu(new LoginMenu());
         while (!menuStack.isEmpty()) {
             menuStack.runTopMenu();
