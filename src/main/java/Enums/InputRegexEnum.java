b9ef767e0026115c068a44dc9241e112e17787b2
alierzakazemeini <alierzakazemeini@ce.sharif.edu>
2022-04-20 02:15:03 +0430

    new method for parsing string

--- src/main/java/Enums/InputRegexEnum.java
+++ src/main/java/Enums/InputRegexEnum.java
@@ -1,12 +1,14 @@
 package Enums;
 
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum InputRegexEnum {
     LOGIN("[\\s]*user[\\s]+login[\\s]+(?<part1>[-]{1,2}.[\\s]+)[\\s]+(?<part2>[.[\\S]]+)[\\s]+(?<part3>[-]{1,2}[.[\\s]]+)[\\s]+(?<part2>[.[\\S]]+)[\\s]*"),
     REGISTER(""),
-    PLAY_GAME_WITH(""),
+    PLAY_GAME_WITH("[\\s]*play game (?<player>(--player|-p)[0-9]+ [.\\S]+[\\s]*)+"),
+    PARSE_PLAY_GAME_WITH("[\\s]*(--player|-p)(?<turn>[0-9]+) (?<username>[.\\S]+)[\\s]*"),
     INFO(""),
     EXIT(""),
     BACK(""),
@@ -23,60 +25,81 @@ public enum InputRegexEnum {
         this.regex = input;
     }
 
-    public static Matcher inputMatcher(String input, InputRegexEnum check) {
+    private TreeMap<Integer, String> map = new TreeMap<>();
+
+    public void inputMatcher(String input, InputRegexEnum check) {
         Matcher matcher = Pattern.compile(check.regex).matcher(input);
         if (!matcher.find() || !matcher.group().equals(input)) {
-            return null;
+            returnOptions(new StringBuilder().append(input));
         }
-        return returnOptions(matcher);
     }
 
-    private static Matcher returnOptions(Matcher givenMatcher) {
+    private TreeMap<Integer, String> returnOptions(StringBuilder input) {
+        if (regex.startsWith("play game")) {
+            return playGameWithMatcher(input);
+        }
+        return null;
+    }
 
+    private Matcher registerMatcher(Matcher givenMatcher) {
         return givenMatcher;
     }
 
-    private static Matcher registerMatcher(Matcher givenMatcher) {
+    private Matcher loginMatcher(Matcher givenMatcher) {
         return givenMatcher;
     }
 
-    private static Matcher loginMatcher(Matcher givenMatcher) {
+    private TreeMap<Integer, String> playGameWithMatcher(StringBuilder input) {
+        Matcher matcher = Pattern.compile(PLAY_GAME_WITH.regex).matcher(input.toString());
+        if (matcher.find() && matcher.group().equals(input.toString())) {
+            int index = input.indexOf(matcher.group("player"));
+            input.delete(index, input.length());
+            Matcher userCred;
+            if ((userCred = selectUsersMatcher(matcher.group("player"))) != null) {
+                map.put(Integer.parseInt(userCred.group("turn")), userCred.group("username"));
+                return playGameWithMatcher(input);
+            }
+        }
+        return map;
+    }
+
+    private Matcher selectUsersMatcher(String input) {
+        Matcher matcher = Pattern.compile(PARSE_PLAY_GAME_WITH.regex).matcher(input);
+        if (matcher.find() && matcher.group().equals(input)) {
+            return matcher;
+        }
+        return null;
+    }
+
+    private Matcher infoMatcher(Matcher givenMatcher) {
         return givenMatcher;
     }
 
-    private static Matcher playGameWithMatcher(Matcher givenMatcher) {
+    private Matcher exitMatcher(Matcher givenMatcher) {
         return givenMatcher;
     }
 
-    private static Matcher infoMatcher(Matcher givenMatcher) {
+    private Matcher changePassMatcher(Matcher givenMatcher) {
         return givenMatcher;
     }
 
-    private static Matcher exitMatcher(Matcher givenMatcher) {
+    private Matcher changeNameMatcher(Matcher givenMatcher) {
         return givenMatcher;
     }
 
-    private static Matcher changePassMatcher(Matcher givenMatcher) {
+    private Matcher logoutMatcher(Matcher givenMatcher) {
         return givenMatcher;
     }
 
-    private static Matcher changeNameMatcher(Matcher givenMatcher) {
+    private Matcher currentMenuMatcher(Matcher givenMatcher) {
         return givenMatcher;
     }
 
-    private static Matcher logoutMatcher(Matcher givenMatcher) {
+    private Matcher enterMenuMatcher(Matcher givenMatcher) {
         return givenMatcher;
     }
 
-    private static Matcher currentMenuMatcher(Matcher givenMatcher) {
-        return givenMatcher;
-    }
-
-    private static Matcher enterMenuMatcher(Matcher givenMatcher) {
-        return givenMatcher;
-    }
-
-    private static Matcher exitMenuMatcher(Matcher givenMatcher) {
+    private Matcher exitMenuMatcher(Matcher givenMatcher) {
         return givenMatcher;
     }
 }
--- src/main/java/Models/Units/CombatUnit.java
+++ src/main/java/Models/Units/CombatUnit.java
@@ -1,14 +1,13 @@
 package Models.Units;
 
-import Enums.GameEnums.ResourceEnum;
-import Enums.GameEnums.TechnologyEnum;
-
-import java.util.ArrayList;
-
 public class CombatUnit extends Unit {
-    protected ArrayList<ResourceEnum> requiredResources = new ArrayList<>();
-    protected ArrayList<TechnologyEnum> requiredTechs = new ArrayList<>();
-    protected String combatType;
-    protected int combatStrength;
-    protected int range;
+    public void attack() {
+
+    }
+
+    public void mount() {
+
+    }
+
+
 }
