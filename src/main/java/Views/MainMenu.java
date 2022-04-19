0e5ff00076931c5aa1641c1668cbb9f21c761117
alierzakazemeini <alierzakazemeini@ce.sharif.edu>
2022-04-19 13:55:16 +0430

    MainMenu functions were added

--- src/main/java/Views/MainMenu.java
+++ src/main/java/Views/MainMenu.java
@@ -1,6 +1,10 @@
 package Views;
 
 import Controllers.Command;
+import Controllers.LoginMenuController;
+import Enums.CommandResponseEnum;
+
+import java.util.List;
 
 
 public class MainMenu extends Menu {
@@ -9,8 +13,18 @@ public class MainMenu extends Menu {
         switch (command.getType()) {
             case "show current menu" -> System.out.println("Main Menu");
             case "menu exit" -> MenuStack.getInstance().popMenu();
-            case "goto game menu" -> MenuStack.getInstance().pushMenu(new GameMenu());
+            case "play game" -> this.playGame(command);
             case "goto profile menu" -> MenuStack.getInstance().pushMenu(new ProfileMenu());
         }
+    }
+
+    private void playGame(Command command) {
+        CommandResponseEnum response = command.validateOptions(List.of("username"));
+        if (!response.isOK()) System.out.println(response);
+        else {
+            String username = command.getOption("username");
+            System.out.println(!response.isOK() ? response : "user created successfully");
+        }
+        MenuStack.getInstance().pushMenu(new GameMenu());
     }
 }
