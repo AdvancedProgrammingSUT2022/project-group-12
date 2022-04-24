82abb4b6751658eb87e202092a095e222aab07b8
AM1648 <a.mjahromy1648@gmail.com>
2022-04-24 12:47:47 +0430

    commandParser improved

--- src/main/java/Exceptions/InvalidCommandFormat.java
+++ src/main/java/Exceptions/InvalidCommandFormat.java
@@ -4,5 +4,9 @@ public class InvalidCommandFormat extends CommandException {
     public InvalidCommandFormat() {
         super("command format is invalid");
     }
+
+    public InvalidCommandFormat(int pos) {
+        super("command format is invalid, at pos " + (pos + 1)); // one-base index
+    }
 }
 
