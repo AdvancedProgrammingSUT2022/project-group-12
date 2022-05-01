0fceae5bdc3a2e45236fe1e0222fb86d44560e66
alirezakazemeini <alirezakazemeini@ce.sharif.edu>
2022-05-01 11:32:21 +0430

    Terrain Features in enum sorted out

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Exception in thread "main" java.lang.NullPointerException: Cannot invoke "java.util.HashMap.entrySet()" because "this.users" is null
         Database.getInstance().deserialize();
         Scanner scanner = new Scanner(System.in);
         MenuStack menuStack = MenuStack.getInstance();
