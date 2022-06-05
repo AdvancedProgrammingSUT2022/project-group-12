bd998aa0a8aefd7652e50bfe5f5af3a07c7fa9ce
alirezakazemeini <alirezakazemeini@ce.sharif.edu>
2022-06-05 19:07:20 +0430

    avatar selection improved

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,11 +7,10 @@ module project {
     requires com.google.gson;
     requires annotations;
 
-    opens Project.Models to com.google.gson;
-    exports Project.Models;
-
     opens Project to javafx.fxml;
     exports Project;
     exports Project.Views;
     opens Project.Views to javafx.fxml;
+    exports Project.Models;
+    opens Project.Models;
 }
