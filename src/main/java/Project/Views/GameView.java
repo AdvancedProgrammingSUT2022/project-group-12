b1c456e08c63b8303dabe531564c303969f56433
alirezakazemeini <alirezakazemeini@ce.sharif.edu>
2022-06-25 19:42:00 +0430

    hex concept for games added

--- src/main/java/Project/Views/GameView.java
+++ src/main/java/Project/Views/GameView.java
@@ -1,11 +1,14 @@
 package Project.Views;
 
+import Project.Models.Tiles.Hex;
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
-public class GameView {
+public class GameView implements ViewController{
     @FXML
     public GridPane tilePane;
+    public Pane tempPane;
     @FXML
     private GridPane buildingsPane;
     @FXML
