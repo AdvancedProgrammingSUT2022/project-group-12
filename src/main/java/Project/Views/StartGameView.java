496d2fbafe98e0c0188da1b0fcb18095c3ea08de
alirezakazemeini <alirezakazemeini@ce.sharif.edu>
2022-07-05 22:06:42 +0430

    game select menu improved

--- src/main/java/Project/Views/StartGameView.java
+++ src/main/java/Project/Views/StartGameView.java
@@ -2,14 +2,14 @@ package Project.Views;
 
 import Project.Controllers.MainMenuController;
 import Project.Models.Database;
-import Project.Models.Game;
 import Project.Models.User;
 import Project.Utils.CommandException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -18,7 +18,13 @@ import java.util.ArrayList;
 public class StartGameView implements ViewController {
     private final ArrayList<String> users = new ArrayList<>();
     @FXML
+    private Spinner<Integer> gridSizeHeight;
+    @FXML
+    private Spinner<Integer> gridSizeWidth;
+    @FXML
     private Button acceptButton;
+    private int currentWidthSize;
+    private int currentHeightSize;
     private ArrayList<String> selectedUsernames;
     @FXML
     private VBox userBox1;
@@ -27,9 +33,26 @@ public class StartGameView implements ViewController {
     @FXML
     private ChoiceBox<String> userSelect;
     private ArrayList<String> usernames = new ArrayList<>();
+    private SpinnerValueFactory<Integer> widthValueFactory;
+    private SpinnerValueFactory<Integer> heightValueFactory;
 
     public void initialize() {
+        currentWidthSize = 0;
         selectedUsernames = new ArrayList<>();
+        widthValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(10, 999);
+        widthValueFactory.setValue(10);
+        heightValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(10, 999);
+        heightValueFactory.setValue(10);
+        gridSizeWidth.setValueFactory(widthValueFactory);
+        gridSizeWidth.valueProperty().addListener((observableValue, integer, t1) -> {
+            currentWidthSize = gridSizeWidth.getValue();
+            System.out.println(currentWidthSize);
+        });
+        gridSizeHeight.setValueFactory(heightValueFactory);
+        gridSizeHeight.valueProperty().addListener((observableValue, integer, t1) -> {
+            currentHeightSize = gridSizeHeight.getValue();
+            System.out.println(currentHeightSize);
+        });
         usernames = Database.getInstance().getAllUsers();
         usernames.remove(MenuStack.getInstance().getUser().getUsername());
         userSelect.getItems().removeAll(userSelect.getItems());
@@ -46,6 +69,12 @@ public class StartGameView implements ViewController {
         selectedUsernames.add(userSelect.getValue());
         userBox1.getChildren().add(returnText(userSelect.getValue()));
         userSelect.getItems().remove(userSelect.getValue());
+        currentWidthSize += 10;
+        currentHeightSize += 10;
+        widthValueFactory.setValue(currentWidthSize);
+        heightValueFactory.setValue(currentHeightSize);
+        gridSizeWidth.setValueFactory(widthValueFactory);
+        gridSizeHeight.setValueFactory(heightValueFactory);
     }
 
     private Text returnText(String value) {
@@ -54,6 +83,12 @@ public class StartGameView implements ViewController {
             userBox1.getChildren().remove(newText);
             userSelect.getItems().add(value);
             users.remove(value);
+            currentWidthSize -= 10;
+            currentHeightSize -= 10;
+            widthValueFactory.setValue(currentWidthSize);
+            heightValueFactory.setValue(currentHeightSize);
+            gridSizeWidth.setValueFactory(widthValueFactory);
+            gridSizeHeight.setValueFactory(heightValueFactory);
         });
         return newText;
     }
@@ -65,6 +100,7 @@ public class StartGameView implements ViewController {
 
     public void acceptClick() {
         try {
+            //todo : set grid with and height
             MainMenuController.startNewGame(selectedUsernames);
         } catch (CommandException e) {
             System.err.println("error in start game/ accept click");
