41000710bc8d2f84a63ae40420f80a2b5ce08acd
AM1648 <a.mjahromy1648@gmail.com>
2022-07-09 10:27:06 +0430

    minor fix

--- src/main/java/Project/Models/Game.java
+++ src/main/java/Project/Models/Game.java
@@ -155,8 +155,7 @@ public class Game {
     }
 
     public Civilization getCurrentCivilization() {
-        //todo fix
-        return civilizations.get(this.gameTurn % civilizations.size() + 1);
+        return civilizations.get(this.gameTurn % civilizations.size());
     }
 
     public void startNewTurn() throws GameException {
