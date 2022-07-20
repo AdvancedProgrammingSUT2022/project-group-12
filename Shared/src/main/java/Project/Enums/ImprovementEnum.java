5334c02ea0c6fa2d3ef3b8891fa33c315f7fdf16
Alireza Askarian <ar.asgarian1389@gmail.com>
2022-07-20 18:40:50 +0430

    Feature enums removed from improvement

--- Shared/src/main/java/Project/Enums/ImprovementEnum.java
+++ Shared/src/main/java/Project/Enums/ImprovementEnum.java
@@ -7,61 +7,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum ImprovementEnum {
-    RESET(new ArrayList<>(), new ArrayList<>(), new ArrayList<>()),
+    RESET(new ArrayList<>(), new ArrayList<>()  ),
     ROAD(
             new ArrayList<>(List.of(TechnologyEnum.THE_WHEEL)),
-            new ArrayList<>(List.of(TerrainEnum.PLAIN, TerrainEnum.DESERT, TerrainEnum.GRASSLAND, TerrainEnum.TUNDRA, TerrainEnum.HILL, TerrainEnum.SNOW)),
-            new ArrayList<>(List.of(FeatureEnum.JUNGLE, FeatureEnum.MARSH, FeatureEnum.FOREST, FeatureEnum.RIVER))),
+            new ArrayList<>(List.of(TerrainEnum.PLAIN, TerrainEnum.DESERT, TerrainEnum.GRASSLAND, TerrainEnum.TUNDRA, TerrainEnum.HILL, TerrainEnum.SNOW))),
     // todo: rethink about
     RAILROAD(
             new ArrayList<>(List.of(TechnologyEnum.RAILROAD)),
-            new ArrayList<>(List.of(TerrainEnum.PLAIN, TerrainEnum.DESERT, TerrainEnum.GRASSLAND, TerrainEnum.TUNDRA, TerrainEnum.HILL, TerrainEnum.SNOW)),
-            new ArrayList<>(List.of(FeatureEnum.JUNGLE, FeatureEnum.MARSH, FeatureEnum.FOREST, FeatureEnum.RIVER))),
+            new ArrayList<>(List.of(TerrainEnum.PLAIN, TerrainEnum.DESERT, TerrainEnum.GRASSLAND, TerrainEnum.TUNDRA, TerrainEnum.HILL, TerrainEnum.SNOW))),
     CAMP(
             new ArrayList<>(List.of(TechnologyEnum.TRAPPING)),
-            new ArrayList<>(List.of(TerrainEnum.TUNDRA, TerrainEnum.PLAIN, TerrainEnum.HILL)),
-            new ArrayList<>(List.of(FeatureEnum.FOREST))),
+            new ArrayList<>(List.of(TerrainEnum.TUNDRA, TerrainEnum.PLAIN, TerrainEnum.HILL))
+            ),
     FARM(
             new ArrayList<>(List.of(TechnologyEnum.AGRICULTURE)),
-            new ArrayList<>(List.of(TerrainEnum.GRASSLAND, TerrainEnum.PLAIN, TerrainEnum.DESERT)),
-            new ArrayList<>()),
+            new ArrayList<>(List.of(TerrainEnum.GRASSLAND, TerrainEnum.PLAIN, TerrainEnum.DESERT))
+             ),
     LUMBER_MILL(
             new ArrayList<>(List.of(TechnologyEnum.ENGINEERING)),
-            new ArrayList<>(),
-            new ArrayList<>(List.of(FeatureEnum.FOREST))),
+            new ArrayList<>()),
     MINE(
             new ArrayList<>(List.of(TechnologyEnum.MINING)),
-            new ArrayList<>(List.of(TerrainEnum.GRASSLAND, TerrainEnum.PLAIN, TerrainEnum.DESERT, TerrainEnum.TUNDRA, TerrainEnum.SNOW)),
-            new ArrayList<>()),
+            new ArrayList<>(List.of(TerrainEnum.GRASSLAND, TerrainEnum.PLAIN, TerrainEnum.DESERT, TerrainEnum.TUNDRA, TerrainEnum.SNOW))
+             ),
     PASTURE(new ArrayList<>(List.of(TechnologyEnum.ANIMAL_HUSBANDRY)),
-            new ArrayList<>(List.of(TerrainEnum.GRASSLAND, TerrainEnum.PLAIN, TerrainEnum.DESERT, TerrainEnum.TUNDRA, TerrainEnum.HILL)),
-            new ArrayList<>()),
+            new ArrayList<>(List.of(TerrainEnum.GRASSLAND, TerrainEnum.PLAIN, TerrainEnum.DESERT, TerrainEnum.TUNDRA, TerrainEnum.HILL))
+             ),
     QUARRY(
             new ArrayList<>(),
-            new ArrayList<>(List.of(TerrainEnum.DESERT, TerrainEnum.PLAIN, TerrainEnum.GRASSLAND, TerrainEnum.TUNDRA, TerrainEnum.HILL)),
-            new ArrayList<>()),
+            new ArrayList<>(List.of(TerrainEnum.DESERT, TerrainEnum.PLAIN, TerrainEnum.GRASSLAND, TerrainEnum.TUNDRA, TerrainEnum.HILL))
+             ),
     PLANTATION(
             new ArrayList<>(List.of(TechnologyEnum.CALENDAR)),
-            new ArrayList<>(List.of(TerrainEnum.DESERT, TerrainEnum.PLAIN, TerrainEnum.GRASSLAND)),
-            new ArrayList<>(List.of(FeatureEnum.FOREST, FeatureEnum.JUNGLE, FeatureEnum.MARSH, FeatureEnum.FALLOUT))),
+            new ArrayList<>(List.of(TerrainEnum.DESERT, TerrainEnum.PLAIN, TerrainEnum.GRASSLAND))),
     TRADING_POST(
             new ArrayList<>(List.of(TechnologyEnum.TRAPPING)),
-            new ArrayList<>(List.of(TerrainEnum.GRASSLAND, TerrainEnum.PLAIN, TerrainEnum.DESERT, TerrainEnum.TUNDRA)),
-            new ArrayList<>()),
+            new ArrayList<>(List.of(TerrainEnum.GRASSLAND, TerrainEnum.PLAIN, TerrainEnum.DESERT, TerrainEnum.TUNDRA))),
 
     FORT(
             new ArrayList<>(List.of(TechnologyEnum.ENGINEERING)),
-            new ArrayList<>(List.of(TerrainEnum.DESERT, TerrainEnum.PLAIN, TerrainEnum.GRASSLAND, TerrainEnum.TUNDRA, TerrainEnum.SNOW)),
-            new ArrayList<>());
+            new ArrayList<>(List.of(TerrainEnum.DESERT, TerrainEnum.PLAIN, TerrainEnum.GRASSLAND, TerrainEnum.TUNDRA, TerrainEnum.SNOW)));
 
     private final ArrayList<TechnologyEnum> requiredTechs;
     private final ArrayList<TerrainEnum> canBeBuiltOn;
-    private final ArrayList<FeatureEnum> features;
 
-    ImprovementEnum(ArrayList<TechnologyEnum> requiredTechs, ArrayList<TerrainEnum> canBeBuiltOn, ArrayList<FeatureEnum> features) {
+    ImprovementEnum(ArrayList<TechnologyEnum> requiredTechs, ArrayList<TerrainEnum> canBeBuiltOn) {
         this.requiredTechs = requiredTechs;
         this.canBeBuiltOn = canBeBuiltOn;
-        this.features = features;
     }
 
 
