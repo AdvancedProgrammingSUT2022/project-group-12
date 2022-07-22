c34e5105add76f509b72f7c5563ff1c16f3ba2c3
alirezakazemeini <alirezakazemeini@ce.sharif.edu>
2022-07-22 20:50:38 +0430

    friend concept improved

--- Client/src/main/java/Client/Views/FriendsView.java
+++ Client/src/main/java/Client/Views/FriendsView.java
@@ -19,16 +19,17 @@ import java.util.ArrayList;
 
 public class FriendsView implements ViewController {
     @FXML
-    private
-    VBox usersBox;
+    private HBox friendAcceptBox;
     @FXML
-    private MenuButton userSelect;
+    private VBox friendsBox;
     @FXML
-    private TextField typeUser;
+    private MenuButton addToFriendsMenu;
     @FXML
-    private MenuButton friendRequests;
+    private TextField typeFriendNameField;
     @FXML
-    private HBox friendAccept;
+    private MenuButton receivedFriendRequests;
+    @FXML
+    private VBox requestsBox;
     private ArrayList<User> users;
     private User selectedUser;
     private MenuItem selectedItem;
@@ -36,75 +37,76 @@ public class FriendsView implements ViewController {
     public void initialize() {
         selectedUser = MenuStack.getInstance().getUser();
         users = DatabaseQuerier.getAllUsers();
-        typeUser.setOnAction(this::startTyping);
+        typeFriendNameField.setOnAction(this::startTyping);
         initUserSelect();
         initFriendRequests();
         initUserBox();
+        initRequestBox();
+    }
+
+    private void initRequestBox() {
+        requestsBox.getChildren().removeAll(requestsBox.getChildren());
+        for (String name : selectedUser.getWaitingOnFriendRequest()){
+            Text username = new Text(name);
+            requestsBox.getChildren().add(username);
+        }
     }
 
     private void startTyping(ActionEvent event) {
-        if (!users.contains(typeUser.getText())) {
-            typeUser.setStyle("-fx-border-color: #ff0066; -fx-border-radius: 5; -fx-border-width: 3;");
+        if (!users.contains(typeFriendNameField.getText())) {
+            typeFriendNameField.setStyle("-fx-border-color: #ff0066; -fx-border-radius: 5; -fx-border-width: 3;");
             return;
         }
-        typeUser.setStyle(null);
-        User user = DatabaseQuerier.getUser(typeUser.getText());
-        user.sendFriendRequest(selectedUser.getUsername());
+        typeFriendNameField.setStyle(null);
+        User user = DatabaseQuerier.getUser(typeFriendNameField.getText());
         DatabaseQuerier.sendFriendRequest(selectedUser.getUsername(), user.getUsername());
-        selectedUser.addToWaitingOnFriendRequest(user.getUsername());
         user.sendFriendRequest(selectedUser.getUsername());
+        initRequestBox();
     }
 
     private void initUserBox() {
         for (String username : selectedUser.getFriends()) {
-            Text name = new Text(username);
-            usersBox.getChildren().add(name);
+            Text friend = new Text(username);
+            friendsBox.getChildren().add(friend);
         }
     }
 
     private void initFriendRequests() {
-        for (String username : selectedUser.getFriendRequest()) {
-            MenuItem item = new MenuItem(username);
-            friendRequests.setText(username);
-            item.setOnAction(actionEvent -> {
-                friendAccept.getChildren().removeAll(friendAccept.getChildren());
-                Text text = new Text(username);
+        for (String request : selectedUser.getFriendRequest()) {
+            MenuItem item = new MenuItem(request);
+            item.setOnAction(event -> {
                 selectedItem = item;
-                friendAccept.getChildren().add(text);
-                friendAccept.getChildren().add(acceptBtn(username));
-                friendAccept.getChildren().add(denialBtn(username));
-                friendAccept.setSpacing(10);
+                friendsBox.getChildren().add(new Text(request));
+                friendsBox.getChildren().add(acceptBtn(request));
+                friendsBox.getChildren().add(denialBtn(request));
+                friendsBox.setSpacing(10);
             });
-            friendRequests.getItems().add(item);
+            receivedFriendRequests.getItems().add(item);
         }
     }
 
     private Button acceptBtn(String username) {
-        User checkedUser = DatabaseQuerier.getUser(username);
         Button button = new Button("Accept");
         button.setBackground(Background.fill(Color.GREEN));
+        button.setOnMouseEntered(actionEvent -> button.setCursor(Cursor.HAND));
         button.setOnMouseClicked(actionEvent -> {
-            button.setCursor(Cursor.HAND);
-            selectedUser.acceptFriend(username);
-            usersBox.getChildren().add(new Text(username));
-            checkedUser.acceptFriend(selectedUser.getUsername());
-            friendRequests.getItems().remove(selectedItem);
-            friendAccept.getChildren().removeAll(friendAccept.getChildren());
+            DatabaseQuerier.acceptFriendRequest(selectedUser.getUsername(), username);
+            friendsBox.getChildren().add(new Text(username));
+            receivedFriendRequests.getItems().remove(selectedItem);
+            friendAcceptBox.getChildren().removeAll(friendAcceptBox.getChildren());
         });
         return button;
     }
 
     private Button denialBtn(String username) {
-        User checkedUser = DatabaseQuerier.getUser(username);
         Button button = new Button("Denial");
         button.setBackground(Background.fill(Color.RED));
+        button.setOnMouseEntered(actionEvent -> button.setCursor(Cursor.HAND));
         button.setOnMouseClicked(actionEvent -> {
-            button.setCursor(Cursor.HAND);
-            DatabaseQuerier.denyFriendRequest(selectedUser.getUsername(), checkedUser.getUsername());
-            selectedUser.denyFriend(username);
-            checkedUser.denyFriend(selectedUser.getUsername());
-            friendRequests.getItems().remove(selectedItem);
-            friendAccept.getChildren().removeAll(friendAccept.getChildren());
+            DatabaseQuerier.denyFriendRequest(selectedUser.getUsername(), username);
+            friendsBox.getChildren().add(new Text(username));
+            receivedFriendRequests.getItems().remove(selectedItem);
+            friendAcceptBox.getChildren().removeAll(friendAcceptBox.getChildren());
         });
         return button;
     }
@@ -113,13 +115,11 @@ public class FriendsView implements ViewController {
         for (User user : users) {
             MenuItem item = new MenuItem(user.getUsername());
             item.setOnAction(actionEvent -> {
-                userSelect.setText(user.getUsername());
-                user.sendFriendRequest(selectedUser.getUsername());
+                addToFriendsMenu.setText(user.getUsername());
                 DatabaseQuerier.sendFriendRequest(selectedUser.getUsername(), user.getUsername());
-                selectedUser.addToWaitingOnFriendRequest(user.getUsername());
-                user.sendFriendRequest(selectedUser.getUsername());
+                initRequestBox();
             });
-            userSelect.getItems().add(item);
+            addToFriendsMenu.getItems().add(item);
         }
     }
 
